951e29fc2914a9b4974d96e251f5fdfcf7fefde0
Mmedara <--replace-all>
2023-04-26 12:06:52 -0400

    Set up sign up and sign in uis

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Link,
   Outlet, 
   useRoutes,
-  useNavigate
+  useNavigate,
+  useLocation
 } from "react-router-dom";
 import { 
     EmployerDiv, H1, Hamburger, ItemsDiv, LogoDiv, MainDiv, Menu, MenuItem, 
@@ -14,19 +15,33 @@ import { FaBars } from 'react-icons/fa';
 
 const NavBar = (props) => {
     const [showMenu, setShowMenu] = useState(false);
-
+    const navigate = useNavigate();
+    const location = useLocation();
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedLoginStatus = localStorage.getItem('userId');
+        if (storedLoginStatus === 'true') {
+          setIsLoggedIn(true);
+        }
+    }, []);
 
   return (
     <MainDiv style={{backgroundColor: "#FFFFFF"}}>
-        <LogoDiv>
+        <LogoDiv onClick={props.navToHome}>
             <H1>LOGO</H1>
         </LogoDiv>
         <ItemsDiv>
-            <VolunteerDiv><P>Find Volunteer Work</P></VolunteerDiv>
-            <SignInDiv><P>Sign In</P></SignInDiv>
+            <VolunteerDiv onClick={props.navToHome}><P>Find Volunteer Work</P></VolunteerDiv>
+            {
+                !isLoggedIn &&
+                location.pathname=="/signin"?
+                (<SignInDiv onClick={props.navToSignUp}><P>Sign Up</P></SignInDiv>):
+                (<SignInDiv onClick={props.navToSignIn}><P>Sign In</P></SignInDiv>)
+            }
         </ItemsDiv>
         <EmployerDiv>
             <P>Employers / Post Job</P>
@@ -35,9 +50,15 @@ const NavBar = (props) => {
             <FaBars/>
         </Hamburger>
         <Menu style={{ display: showMenu ? 'flex' : 'none' }}>
-            <MenuItem>Find Volunteer Work</MenuItem>
-            <MenuItem>Sign In</MenuItem>
-            <MenuItem>Employers / Post Job</MenuItem>
+            <MenuItem onClick={props.navToHome}><P>Find Volunteer Work</P></MenuItem>
+            {
+                !isLoggedIn &&
+                location.pathname=="/signin"?
+                (<MenuItem onClick={props.navToSignUp}><P>Sign Up</P></MenuItem>):
+                (<MenuItem onClick={props.navToSignIn}><P>Sign In</P></MenuItem>)
+            }
+            <MenuItem><P>Employers / Post Job</P></MenuItem>
+            <MenuItem><P>Sign Out</P></MenuItem>
         </Menu>
     </MainDiv>
   )
